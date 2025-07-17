08d6f7881767a59a7919741bd701485de69d183e
niasua <nsuar@DESKTOP-G227FH7>
2025-07-17 17:43:48 -0300

    Create the flashcards and stack models

---
+++ Flashcards.Niasua/Models/Flashcard.cs
@@ -0,0 +1,11 @@
+﻿namespace Flashcards.Niasua.Models
+{
+    public class Flashcard
+    {
+        public int FlashcardId { get; set; }
+        public int StackId { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public Stack Stack { get; set; }
+    }
+}
---
+++ Flashcards.Niasua/Models/Stack.cs
@@ -0,0 +1,8 @@
+﻿namespace Flashcards.Niasua.Models;
+
+internal class Stack
+{
+    public int StackId { get; set; }
+    public string Name { get; set; }
+    public List<Flashcard> Flashcards { get; set; }
+}
