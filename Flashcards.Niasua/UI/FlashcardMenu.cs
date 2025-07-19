3c24fe95aec444b895c5ccd6de6525bb90a8c1ba
niasua <nsuar@DESKTOP-G227FH7>
2025-07-18 21:06:31 -0300

    Menu distribution changed

---
+++ Flashcards.Niasua/UI/FlashcardMenu.cs
@@ -0,0 +1,76 @@
+﻿using Flashcards.Niasua.Services;
+using Spectre.Console;
+using System.Data;
+
+namespace Flashcards.Niasua.UI;
+
+public class FlashcardMenu
+{
+    public static void Show()
+    {
+        while (true)
+        {
+            Console.Clear();
+            var choice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("[green]Flashcards Menu[/]")
+                .AddChoices(new[]
+                {
+                    "Add",
+                    "View",
+                    "Edit",
+                    "Delete",
+                    "Back"
+                }));
+
+            switch (choice)
+            {
+                case "Add":
+
+                    AddFlashcard();
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    public static void AddFlashcard()
+    {
+        while (true)
+        {
+            Console.Clear();
+            AnsiConsole.MarkupLine("[red]Create Flashcard:[/]\n");
+
+            AnsiConsole.MarkupLine("Type the [green]name[/] of the Flashcard Stack (type 'zz' to return to the menu):");
+            var stackName = Console.ReadLine();
+            if (stackName?.ToLower() == "zzz") break;
+
+            AnsiConsole.MarkupLine("\nType the [green]question[/] (type 'zz' to return to the menu):");
+            var question = Console.ReadLine();
+            if (question?.ToLower() == "zzz") break;
+
+            AnsiConsole.MarkupLine("\nType the [green]answer[/] (type 'zz' to return to the menu):");
+            var answer = Console.ReadLine();
+            if (answer?.ToLower() == "zzz") break;
+
+            var creation = FlashcardService.CreateFlashcard(stackName, question, answer);
+
+            if (creation)
+            {
+                AnsiConsole.MarkupLine("\n[green]Flashcard successfully added![/]:");
+            }
+            else
+            {
+                AnsiConsole.MarkupLine("\n[red]The stack was not found[/]:");
+            }
+
+            AnsiConsole.MarkupLine("\nPress any key to add another flashcard or type [red]'zzz'[/] to return.");
+
+            var input = Console.ReadLine();
+            if (input?.ToLower() == "zzz") break;
+        }
+    }
+}
--- Flashcards.Niasua/UI/Menu.cs
+++ Flashcards.Niasua/UI/Menu.cs
@@ -18,22 +18,20 @@ public static class Menu
 
             var choice = AnsiConsole.Prompt(
                 new SelectionPrompt<string>()
-                .Title("Choose an [green]action[/].")
+                .Title("Choose a [green]section[/].")
                 .AddChoices(new[]
                 {
-                    "Add Flashcard",
-                    "View Flashcards",
-                    "Delete Flashcard",
-                    "Edit Flashcard",
-                    "Set Study Session",
-                    "View Sessions"
+                    "Flashcards",
+                    "Stacks",
+                    "Study Sessions",
+                    "Exit"
                 }));
 
             switch (choice)
             {
-                case "Add Flashcard":
+                case "Flashcards":
 
-                    AddFlashcard();
+                    FlashcardMenu.Show();
 
                     break;
 
@@ -41,43 +39,6 @@ public static class Menu
                     break;
             }
 
-        }
-    }
-
-    public static void AddFlashcard()
-    {
-        while (true)
-        {
-            Console.Clear();
-            AnsiConsole.MarkupLine("[red]Create Flashcard:[/]\n");
-
-            AnsiConsole.MarkupLine("Type the [green]name[/] of the Flashcard Stack (type 'zz' to return to the menu):");
-            var stackName = Console.ReadLine();
-            if (stackName?.ToLower() == "zzz") break;
-
-            AnsiConsole.MarkupLine("\nType the [green]question[/] (type 'zz' to return to the menu):");
-            var question = Console.ReadLine();
-            if (question?.ToLower() == "zzz") break;
-
-            AnsiConsole.MarkupLine("\nType the [green]answer[/] (type 'zz' to return to the menu):");
-            var answer = Console.ReadLine();
-            if (answer?.ToLower() == "zzz") break;
-
-            var creation = FlashcardService.CreateFlashcard(stackName, question, answer);
-
-            if (creation)
-            {
-                AnsiConsole.MarkupLine("\n[green]Flashcard successfully added![/]:");
-            }
-            else
-            {
-                AnsiConsole.MarkupLine("\n[red]The stack was not found[/]:");
-            }
-
-            AnsiConsole.MarkupLine("\nPress any key to add another flashcard or type [red]'zzz'[/] to return.");
-
-            var input = Console.ReadLine();
-            if (input?.ToLower() == "zzz") break;
         }
     }
 }
