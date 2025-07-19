b582b357509c9a92f6add97432172c9a9be5d61c
niasua <nsuarez2410@gmail.com>
2025-07-19 20:33:28 -0300

    add the menu structure of study sessions

---
+++ Flashcards.Niasua/UI/StudySessionMenu.cs
@@ -0,0 +1,56 @@
+﻿using Spectre.Console;
+
+namespace Flashcards.Niasua.UI;
+
+public class StudySessionMenu
+{
+    public static void Show()
+    {
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.Clear();
+            var choice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("[green]Study Sessions Menu[/]")
+                .AddChoices(new[]
+                {
+                    "Start a Study Session",
+                    "View Study Sessions History",
+                    "Back to Main Menu
+                }));
+
+            switch (choice)
+            {
+                case "Start a Study Session":
+
+                    CreateStudySession();
+
+                    break;
+
+                case "View Study Sessions History":
+
+                    ViewHistory();
+
+                    break;
+
+                case "Back to Main Menu":
+
+                    exit = true;
+
+                    break;
+            }
+        }
+    }
+
+    private static void CreateStudySession()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void ViewHistory()
+    {
+        throw new NotImplementedException();
+    }
+}
